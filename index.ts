4bb117545ecebcc97c84ab804da8875bb8ff54c1
winter <himj131@gmail.com>
2023-10-12 23:12:26 +0900

    4.2 메서드 추출(5줄제한 규칙)

--- index.ts
+++ index.ts
@@ -163,7 +163,15 @@ function updateMap() {
 function drawMap(g: CanvasRenderingContext2D) {
   for (let y = 0; y < map.length; y++) {
     for (let x = 0; x < map[y].length; x++) {
-      if (map[y][x] === Tile.FLUX)
+      colorOfTitle(g, x, y);
+      if (map[y][x] !== Tile.AIR && map[y][x] !== Tile.PLAYER)
+        g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+    }
+  }
+}
+
+function colorOfTitle(g: CanvasRenderingContext2D, x: number, y: number) {
+  if (map[y][x] === Tile.FLUX)
         g.fillStyle = "#ccffcc";
       else if (map[y][x] === Tile.UNBREAKABLE)
         g.fillStyle = "#999999";
@@ -175,11 +183,6 @@ function drawMap(g: CanvasRenderingContext2D) {
         g.fillStyle = "#ffcc00";
       else if (map[y][x] === Tile.KEY2 || map[y][x] === Tile.LOCK2)
         g.fillStyle = "#00ccff";
-
-      if (map[y][x] !== Tile.AIR && map[y][x] !== Tile.PLAYER)
-        g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
-    }
-  }
 }
 
 function draw() {
